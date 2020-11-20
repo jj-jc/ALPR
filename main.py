e77e7e72520dc274758e8d71908e61c0e61ae416
Juanjojc <juanjojuradocaminod.v@gmail.com>
2020-11-20 16:12:41 +0100

    Se ha utilizado la herramienta de benito para comparar varias imágenes a la vez, y poder escoger el mejor edge detection

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import cv2
 import glob
 import numpy as np
 
+
+CLEAN=True
+
 # function for image stacking for better visualization (no hace falta usarla, la he sacado de por ahí)
 def stackImages(scale,imgArray):
     rows = len(imgArray)
@@ -62,6 +65,10 @@ for image in image_filenames:
     image_list.append(cv2.imread(image))
 
 
+
+
+################################License plate detection################################
+######################################################################################
 # Image convert to gray scale
 image_gray = []
 for gray in image_list:
@@ -88,60 +95,82 @@ for binarized in image_equalized:
 # Laplacian Edge Detection, Canny can be used too (Si quieres, puedes intentar hacer funcionar el de Sobel bien
 # con otros parámetros, a mi me sale horrible)
 image_edge = []
+image_edge_canny=[]
+image_edge_sobel=[]
 #filter = np.array([-1, 0, 1],[-2, 0, 2],[-1, 0, 1])
 for edge in image_binarized:
-    #image_edge.append(cv2.Canny(edge, 100, 100))
-    image_edge.append(cv2.Laplacian(edge, cv2.CV_8UC1))
-    #image_edge.append(cv2.Sobel(edge, cv2.CV_8UC1, 1, 1, ksize=5))
+    image_edge.append(cv2.Canny(edge, 300, 300))
+    image_edge_canny.append(cv2.Laplacian(edge, cv2.CV_8UC1))
+    image_edge_sobel.append(cv2.Sobel(edge, cv2.CV_8UC1, 1, 1, ksize=5))
 
-# Edge dilation (Realmente no está puesta ninguna dilation porque el kernel es (1, 1), puedes probar si con (2, 2) va
-# mejor, desde luego más de (2, 2) es una burrada), (el erode empeora más que mejorar, y el resto de operaciones
-# morfologicas no parecen mejorar tampoco (open, close, tophat, blackhat, etc))
-image_dilated = []
-# image_eroded = []
-dilate_kernel = np.ones((1, 1), np.uint8)
-erode_kernel = np.ones((2, 2), np.uint8)
-for dilate in image_edge:
-    image_dilated.append(cv2.dilate(dilate, dilate_kernel, iterations=1))
-    #image_dilated.append(cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)))
-    a = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-    #cv2.imshow("LUL", cv2.getStructuringElement(cv2.MORPH_CROSS, (5, 5)))
-    cv2.waitKey(0)
-# for erode in image_dilated:
-#     image_eroded.append(cv2.erode(erode, erode_kernel, iterations=1))
+##------------------------- TO COMPARE THE PREPROCESSING ---------------##
+i=0;
+while True:
+    comparative_imag = stackImages(0.6,([image_binarized[i],image_edge[i]],[image_edge_canny[i],image_edge_sobel[i]]))
+    cv2.imshow("comparativa: "+ image_filenames[i],comparative_imag)
+    k = cv2.waitKey(0)                  # wait for a key 
+    if k == 27:                         # 'ESC' for exist
+        cv2.destroyAllWindows() 
+        break;
+    elif k == ord('n'):                  # 'n' for next
+        i+=1        
+    elif k == ord('b'):                  # 'b' for back
+        i-=1
+    if (CLEAN == True):
+        cv2.destroyAllWindows()
 
-# Contour detection
-image_contour = []
-for index in range(0, len(image_list)):
-    original = image_dilated[index] # crea una copia de la imagen preprocesada para trabajar sobre ella
-    cont, hier = cv2.findContours(original, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) # encuentra los
-    # contornos de la imagen y los almacena en un vector "cont". La variable "hier" no la usamos porque tiene que ver
-    # con la jerarquía que toman los contornos entre ellos.
-    all_contours = image_list[index].copy()
-    cv2.drawContours(all_contours, cont, -1, (255, 0, 0), 3) # estas dos líneas junto con la siguiente que está
-    # comentada muestran los contornos detectados sobre la imagen original para visualización
-    #cv2.imshow("contours", all_contours)
-    cont = sorted(cont, key=cv2.contourArea, reverse=True)[:8] # esto sirve para obtener los 8 contornos con más área
-    # y así no tener en cuenta contornos muy pequeños que puede haber en la imagen. A partir de estos 8 contornos o los
-    # que se se desee se realiza la búsqueda de la matrícula
-    for i in cont:
-        perimeter = cv2.arcLength(i, True)
-        lines = cv2.approxPolyDP(i, 0.02*perimeter, True) # estas dos funciones aproximan cada contorno en un
-        # polinomio de "n" líneas
-        if len(lines) == 4: # se estudian los contornos de 4 lineas (cuadrados/rectángulos), que es lo que nos
-            # interesa ya que las matrículas de coche son rectángulos
-            x, y, w, h = cv2.boundingRect(i) # aquí se obtiene un rectángulo que contiene al contorno estudiado
-            crp_image = original[y:y+h, x:x+w] # se recorta la imagen para solo tener el contorno estudiado
-            hor_proj, ver_proj = getProj(crp_image) # aquí se obtienen y se grafican las proyecciones vertical y
-            plotProj(hor_proj, ver_proj, crp_image) # horizontal (igual no sirven para nada o igual las podemos usar)
-            compare = image_edge[index].copy()
-            cv2.rectangle(compare, (x, y), (x+w, y+h), (255, 0, 0), 2) # Imagen auxiliar "compare" sobre la cual se
-            # pinta el rectángulo del contorno
-            #break
-            cv2.imshow("edge image", compare)
-            cv2.imshow("license plate", crp_image)
-            cv2.waitKey(0)
-image_contour.append(crp_image)
+
+# # Edge dilation (Realmente no está puesta ninguna dilation porque el kernel es (1, 1), puedes probar si con (2, 2) va
+# # mejor, desde luego más de (2, 2) es una burrada), (el erode empeora más que mejorar, y el resto de operaciones
+# # morfologicas no parecen mejorar tampoco (open, close, tophat, blackhat, etc))
+# image_dilated = []
+# # image_eroded = []
+# dilate_kernel = np.ones((1, 1), np.uint8)
+# erode_kernel = np.ones((2, 2), np.uint8)
+# for dilate in image_edge:
+#     image_dilated.append(cv2.dilate(dilate, dilate_kernel, iterations=1))
+#     #image_dilated.append(cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)))
+#     a = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+#     #cv2.imshow("LUL", cv2.getStructuringElement(cv2.MORPH_CROSS, (5, 5)))
+#     cv2.waitKey(0)
+# # for erode in image_dilated:
+# #     image_eroded.append(cv2.erode(erode, erode_kernel, iterations=1))
+
+# # Contour detection
+# image_contour = []
+# number=0;
+# for index in range(0, len(image_list)):
+#     original = image_dilated[index] # crea una copia de la imagen preprocesada para trabajar sobre ella
+#     cont, hier = cv2.findContours(original, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) # encuentra los
+#     # contornos de la imagen y los almacena en un vector "cont". La variable "hier" no la usamos porque tiene que ver
+#     # con la jerarquía que toman los contornos entre ellos.
+#     all_contours = image_list[index].copy()
+#     cv2.drawContours(all_contours, cont, -1, (255, 0, 0), 3) # estas dos líneas junto con la siguiente que está
+#     # comentada muestran los contornos detectados sobre la imagen original para visualización
+#     #cv2.imshow("contours", all_contours)
+#     cont = sorted(cont, key=cv2.contourArea, reverse=True)[:8] # esto sirve para obtener los 8 contornos con más área
+#     # y así no tener en cuenta contornos muy pequeños que puede haber en la imagen. A partir de estos 8 contornos o los
+#     # que se se desee se realiza la búsqueda de la matrícula
+#     for i in cont:
+#         perimeter = cv2.arcLength(i, True)
+#         lines = cv2.approxPolyDP(i, 0.02*perimeter, True) # estas dos funciones aproximan cada contorno en un
+#         # polinomio de "n" líneas
+#         if len(lines) == 4: # se estudian los contornos de 4 lineas (cuadrados/rectángulos), que es lo que nos
+#             # interesa ya que las matrículas de coche son rectángulos
+#             x, y, w, h = cv2.boundingRect(i) # aquí se obtiene un rectángulo que contiene al contorno estudiado
+#             crp_image = original[y:y+h, x:x+w] # se recorta la imagen para solo tener el contorno estudiado
+#             hor_proj, ver_proj = getProj(crp_image) # aquí se obtienen y se grafican las proyecciones vertical y
+#             #plotProj(hor_proj, ver_proj, crp_image) # horizontal (igual no sirven para nada o igual las podemos usar)
+#             compare = image_edge[index].copy()
+#             cv2.rectangle(compare, (x, y), (x+w, y+h), (255, 0, 0), 2) # Imagen auxiliar "compare" sobre la cual se
+#             # pinta el rectángulo del contorno
+#             #break
+#             cv2.imshow("edge image: "+image_filenames[index], compare)
+#             #cv2.imshow("license plate: ", crp_image)
+#             cv2.waitKey(0)
+#     #cv2.imshow(image)
+#     #number+=1
+# image_contour.append(crp_image)
 
 # Hay dos posibles caminos que tomar respecto a lo que ha dicho Sergio:
 # El primero sería dejar como está el código (incluso quitando la ecualizacion) y hacer a posteriori otro preprocesado
